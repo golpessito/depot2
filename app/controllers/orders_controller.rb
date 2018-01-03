201b4c89cac5a2eaad76de42893e4a2d99a9882b
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2018-01-03 12:42:59 +0100

    Emailing Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,6 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    puts "#{order_params}"
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
 
@@ -36,6 +35,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module Depot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # You have to permit less secure apps in gmails settings
+    # https://myaccount.google.com/lesssecureapps
+    
+    config.action_mailer.smtp_settings = {
+      address:"smtp.gmail.com",
+      port:587,
+      domain:"gmail.com",
+      authentication: "plain",
+      user_name:"david.ruizdelarosa@gmail.com",
+      password:"1_kira_1",
+      enable_starttls_auto: true
+    }
+
   end
 end
