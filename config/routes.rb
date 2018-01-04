36e7adfc767492a1e218e8d2c0985576b4ced172
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2018-01-04 16:23:32 +0100

    I18N

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,19 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :pay_types
-  resources :orders
-  resources :line_items
-  resources :carts
-  # get 'store/index'
-  root 'store#index', as: 'store_index'
+
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :pay_types
+    resources :orders
+    resources :line_items
+    resources :carts
+    # get 'store/index'
+    root 'store#index', as: 'store_index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
